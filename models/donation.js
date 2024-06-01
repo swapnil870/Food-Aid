4288136aaa60ac972a519686cd7851a1a6e777d3
Swapnil Mishra <Swapnil.mishra870@gmail.com>
2024-06-02 00:56:41 +0530

    Initial commit

--- models/donation.js
+++ models/donation.js
@@ -1,45 +1,45 @@
 const mongoose = require("mongoose");
 
 const donationSchema = new mongoose.Schema({
-	donor: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "users",
-		required: true
-	},
-	agent: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "users",
-	},
-	foodType: {
-		type: String,
-		required: true
-	},
-	quantity: {
-		type: String,
-		required: true
-	},
-	cookingTime: {
-		type: Date,
-		required: true
-	},
-	address: {
-		type: String,
-		required: true
-	},
-	phone: {
-		type: Number,
-		required: true
-	},
-	donorToAdminMsg: String,
-	adminToAgentMsg: String,
-	collectionTime: {
-		type: Date,
-	},
-	status: {
-		type: String,
-		enum: ["pending", "rejected", "accepted", "assigned", "collected"],
-		required: true
-	},
+    donor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    agent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    },
+    foodType: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    cookingTime: {
+        type: Date,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    donorToAdminMsg: String,
+    adminToAgentMsg: String,
+    collectionTime: {
+        type: Date,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "rejected", "accepted", "assigned", "collected"],
+        required: true
+    },
 });
 
 const Donation = mongoose.model("donations", donationSchema);
